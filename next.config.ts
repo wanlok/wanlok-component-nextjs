8b848e7133c79c070ac40aaa1a8990e5e81ae0e0
Robert Wan <wantunglok@gmail.com>
2026-08-08 15:31:41 +1000

    Use a wildcard for allowedDevOrigins to survive DHCP IP changes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.0.155"]
+  allowedDevOrigins: ["192.168.0.*"]
 };
 
 export default nextConfig;
